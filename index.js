1a120e104550636b4745341a2fb328482152fe04
Arkur745 <atmaryan745@gmail.com>
2026-04-28 04:00:18 +0530

    Implement ESLint and Prettier

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
