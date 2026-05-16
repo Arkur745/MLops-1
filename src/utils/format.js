defcf418554de9c738a730529dea4764486a0262
Arkur745 <atmaryan745@gmail.com>
2026-05-16 14:31:16 +0200

    Unnecessary files deleted

--- src/utils/format.js
+++
@@ -1,6 +0,0 @@
-export const formatValidationErrors = errors => {
-  if (!errors || !errors.issues) return 'Validation failed with unknown error';
-  if (Array.isArray(errors.issues))
-    return errors.issues.map(i => i.message).join(', ');
-  return JSON.stringify(errors);
-};
